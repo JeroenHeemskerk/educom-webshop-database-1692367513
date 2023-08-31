7c53ceb8e34f48c88981d4cd5315f386bdb76a9d
Stijn121 <s.engelmoer@ziggo.nl>
2023-08-31 16:43:15 +0200

    opdracht #7 gedaan behalve dat het met de week

--- carts.repository.php
+++ carts.repository.php
@@ -2,8 +2,8 @@
 function GetTop5Products(){
     $conn = ConnectDB();
     try {
-        $sql = "SELECT /*ADDDATE(CURDATE(), INTERVAL -7 DAY)*/ id, productid, COUNT(productid) FROM shoppingcart 
-        WHERE date >= '2023-08-31' GROUP BY productid ORDER BY COUNT(productid) DESC LIMIT 5";
+        $sql = "SELECT id, productid, COUNT(productid) FROM shoppingcart 
+         GROUP BY productid ORDER BY COUNT(productid) DESC LIMIT 5";
         $result = mysqli_query($conn, $sql);
         if (!$result) {
             throw new Exception("geen producten gevonden, SQL: " . $sql . ", Error " . $conn->error());
--- top5.php
+++ top5.php
@@ -3,7 +3,7 @@ function ShowTop5($carts){
     $i = 1;
     foreach ($carts as $cart) {
         $Product = SearchForProductById($cart["productid"]); echo '
-        <p>Welkom bij de 5 best verkochte items van deze week</p>
+        <p>Welkom bij de 5 best verkochte items alle tijden</p>
         <div class="Product">
         <p>plek '; echo $i; echo ' best verkochte product deze week</p>
         <p>naam: ';echo $Product["name"]; echo '</p>
